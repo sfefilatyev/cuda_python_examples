c07c1351961eef60f948dbfcada315c918ccf407
Sergiy Fefilatyev <fsergiy@nvidia.com>
2020-02-23 16:55:50 -0800

    [Chapter3] Add draft of gpu-based madelbrot set generation.

---
+++ gpu_mandelbrot.py
@@ -0,0 +1,40 @@
+"""Visualize Mandelbrot set using GPU."""
+import matplotlib
+# Offline image generation.
+matplotlib.use('Agg')
+from matplotlib import pyplot as plt
+import  numpy as np
+import pycuda.autoinit
+from pycuda import gpuarray
+from pycuda.elementwise import ElementwiseKernel
+from time import time
+
+
+def gpu_mandelbrot(width, height, real_low, real_high, imag_low, imag_high, max_iters):
+    real_vals = np.matrix(np.linspace(real_low, real_high, width), dtype=np.complex64)
+    imag_vals = np.matrix(np.linspace(imag_low, imag_high, height), dtype=np.complex64) * 1j
+    mandelbrot_lattice = np.array(real_vals + imag_vals.transpose(), dtype=np.complex64)
+    for x in range(width):
+        for y in range(height):
+            c = np.complex64(real_vals[x] + imag_vals[y] * 1j)
+            z = np.complex64(0)
+            for i in range(max_iters):
+                z = z**2 + c
+                if(np.abs(z) > 2):
+                    mandelbrot_graph[y, x] = 0
+                    break
+    return mandelbrot_graph
+
+if __name__ == "__main__":
+    t1 = time()
+    mandel = simple_mandelbrot(512, 512, -2, 2, -2, 2, 256)
+    t2 = time()
+    mandel_time = t2 - t1
+    t1 = time()
+    fig = plt.figure(1)
+    plt.imshow(mandel, extent=(-2, 2, -2, 2))
+    plt.savefig('mandelbrot.png', dpi=fig.dpi)
+    t2 = time()
+    dump_time = t2 - t1
+    print('It took {} seconds to calculate the Mandelbrot graph.'.format(mandel_time))
+    print('It tool {} seconds to dump the image.'.format(dump_time))
