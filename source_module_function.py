0bf6afe64525bb200742f7fac99684a725420367
Sergiy Fefilatyev <fsergiy@nvidia.com>
2020-03-23 23:14:57 -0700

    [Chapter4] Add kernel to source module function.

--- source_module_function.py
+++ source_module_function.py
@@ -3,3 +3,11 @@ import pycuda.driver as drv
 import numpy as np
 from pycuda import gpuarray
 from pycuda.compiler import SourceModule
+
+ker = SourceModule("""
+__global__ void scalar_multipy_kernel(float *outvec, float scalar, float* vec)
+{
+    int i = threadIdx.x;
+    outvec[i] = scalar * vec[i];   
+}
+""")
