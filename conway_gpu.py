8a8d923a20eb96d55a409db182e80f27c89f6b5b
Sergiy Fefilatyev <fsergiy@nvidia.com>
2020-03-27 23:03:17 -0700

    [Chapter4] Add Conway-game example implemented on PyCUDA.

---
+++ conway_gpu.py
@@ -0,0 +1,7 @@
+import pycuda.autoinit
+import pycuda.driver as drv
+from pycuda import gpuarray
+from pycuda.compiler import SourceModule
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.animation as animation
