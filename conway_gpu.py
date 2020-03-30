7821a01678c30b103e01c10b900e07e8a328565a
Sergiy Fefilatyev <fsergiy@nvidia.com>
2020-03-30 00:36:23 -0700

    [Chapter4] Add final code to life game example.

--- conway_gpu.py
+++ conway_gpu.py
@@ -17,7 +17,6 @@ import matplotlib.animation as animation
 
 ker = SourceModule(
 """
-/***
 #define _X ( threadIdx.x + blockIdx.x * blockDim.x ) 
 #define _Y ( threadIdx.y + blockIdx.y * blockDim.y )
 
@@ -27,25 +26,20 @@ ker = SourceModule(
 #define _XM(x) ( (x + _WIDTH) % _WIDTH )
 #define _YM(y) ( (y + _HEIGHT) % _HEIGHT )
 
-#define _INDEX (x,y) ( _XM(x) + _YM(y) * _WIDTH )
-***/
-
-#define _X  ( threadIdx.x + blockIdx.x * blockDim.x )
-#define _Y  ( threadIdx.y + blockIdx.y * blockDim.y )
-#define _WIDTH  ( blockDim.x * gridDim.x )
-#define _HEIGHT ( blockDim.y * gridDim.y  )
-#define _XM(x)  ( (x + _WIDTH) % _WIDTH )
-#define _YM(y)  ( (y + _HEIGHT) % _HEIGHT )
-#define _INDEX(x,y)  ( _XM(x)  + _YM(y) * _WIDTH )
+#define _INDEX(x,y) ( _XM(x) + _YM(y) * _WIDTH )
 
 
 // device function that reuturns a living number of neighbors for a given cell.
 __device__ int nbrs(int x, int y, int * in)
 {
-//    return ( in[ _INDEX(x-1, y+1) ] + in[ _INDEX(x-1, y)   ] + in[ _INDEX(x-1, y-1) ] + in[ _INDEX(x, y+1)   ] + in[ _INDEX(x, y-1)   ] + in[ _INDEX(x+1, y+1) ] + in[ _INDEX(x+1, y)   ] + in[ _INDEX(x+1, y-1) ] );
-     return ( in[ _INDEX(x -1, y+1) ] + in[ _INDEX(x-1, y) ] + in[ _INDEX(x-1, y-1) ] \
-                   + in[ _INDEX(x, y+1)] + in[_INDEX(x, y - 1)] \
-                   + in[ _INDEX(x+1, y+1) ] + in[ _INDEX(x+1, y) ] + in[ _INDEX(x+1, y-1) ] );
+    return ( in[ _INDEX(x-1, y+1) ] + \
+             in[ _INDEX(x-1, y)   ] + \
+             in[ _INDEX(x-1, y-1) ] + \
+             in[ _INDEX(x, y+1)   ] + \
+             in[ _INDEX(x, y-1)   ] + \
+             in[ _INDEX(x+1, y+1) ] + \
+             in[ _INDEX(x+1, y)   ] + \
+             in[ _INDEX(x+1, y-1) ] );
 }
 
 __global__ void conway_ker(int* lattice_out, int* lattice)
@@ -83,7 +77,7 @@ __global__ void conway_ker(int* lattice_out, int* lattice)
 conway_ker = ker.get_function("conway_ker")
 
 def update_gpu(frameNum, img, newLattice_gpu, lattice_gpu, N):
-    conway_ker(newLattice_gpu, lattice_gpu, grid=(N/32, N/32, 1), block=(32,32,1))        
+    conway_ker(newLattice_gpu, lattice_gpu, grid=(int(N/32), int(N/32), 1), block=(32,32,1))        
 
     img.set_data(newLattice_gpu.get())
     lattice_gpu[:] = newLattice_gpu[:]
@@ -91,7 +85,7 @@ def update_gpu(frameNum, img, newLattice_gpu, lattice_gpu, N):
 
 if __name__ == '__main__':
     # set lattice size
-    N = 256
+    N = 512
     lattice = np.int32( np.random.choice([1,0], N*N, p=[0.25, 0.75]).reshape(N, N))
     lattice_gpu = gpuarray.to_gpu(lattice)
     newLattice_gpu = gpuarray.empty_like(lattice_gpu)
@@ -102,14 +96,9 @@ if __name__ == '__main__':
     ani = animation.FuncAnimation(fig,
                                   update_gpu,
                                   fargs=(img, newLattice_gpu, lattice_gpu, N, ),
-                                  interval=0,
+                                  interval=1,
                                   frames=1000,
                                   save_count=1000)
     plt.show()
-    
-
-
-
-
-
+   
 
