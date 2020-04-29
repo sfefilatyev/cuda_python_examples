524a708010d53fcef3c8798074c7f780fd881df3
Sergiy Fefilatyev <fsergiy@nvidia.com>
2020-04-28 23:30:58 -0700

    [Chapter7] Complete simple FFT example.

--- chapter7/simple_fft_example.py
+++ chapter7/simple_fft_example.py
@@ -6,3 +6,15 @@ from skcuda import fft
 x = np.asarray(np.random.rand(1000), dtype=np.float32)
 x_gpu = gpuarray.to_gpu(x)
 x_hat = gpuarray.empty_like(x_gpu, dtype=np.complex64)
+
+plan = fft.Plan(x_gpu.shape, np.float32, np.complex64)
+inverse_plan = fft.Plan(x.shape, in_dtype=np.complex64, out_dtype=np.float32)
+
+fft.fft(x_gpu, x_hat, plan)
+fft.ifft(x_hat, x_gpu, inverse_plan, scale=True)
+
+y = np.fft.fft(x)
+
+print('cuFFT matches NumPy FFT: {}'.format(np.allclose(x_hat.get(), y, atol=1e-6)))
+print('cuFFT inverse matches original: {}'.format(np.allclose(x_gpu.get(), x, atol=1e-6)))
+
