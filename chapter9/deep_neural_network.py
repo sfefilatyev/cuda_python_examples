f925afa9329e9f0d374df8d370af297d643f707a
Sergiy Fefilatyev <fsergiy@nvidia.com>
2020-05-16 22:55:57 -0700

    [Chapter 9] Add more code to neural network chapter.

--- chapter9/deep_neural_network.py
+++ chapter9/deep_neural_network.py
@@ -1,3 +1,18 @@
 dense_eval_code='''
 __global__ void dense_eval(int num_outputs, int num_inputs, int relu, int sigmoid, float* w, float* b, float* x, float* y, int batch_size, int w_t, int_bt, float delta)
+{
+    int i = blockDim.x * blockIdx.x + threadIdx.x;
+
+    if (i < num_output)
+    {
+        for (int k = 0; k < batch_size; k++)
+        {
+            double temp = 0.0f;
+            for (int j = 0; j < num_inputs; j++)
+            {
+                
+            }
+        }
+    }
+}
 '''
