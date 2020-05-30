d69c33f350255b81c076516b2ff5f14235807f15
Sergiy Fefilatyev <fsergiy@nvidia.com>
2020-05-29 23:45:55 -0700

    [Chapter10] Add Mandelbrot-revisited again example in CUDA.

---
+++ chapter10/mandelbrot.cu
@@ -0,0 +1,8 @@
+#include <cuda_runtime.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+extern "C" __global__ void mandelbrot_ker(float* lattice, float* mandelbrot_graph, int max_iters, float upper_bound_squared, int lattice_size)
+{
+}
