350b3792e33dbe229467b8f431a7285df1bc4773
Sergiy Fefilatyev <fsergiy@nvidia.com>
2020-06-02 23:32:40 -0700

    [Chapter10] Add pxt-based external calls to mandelbrot example.

---
+++ chapter10/mandelbrot_ptx.py
@@ -0,0 +1,8 @@
+from __future__ import division
+from time import time
+import matplotlib
+from matplotlib import pyplot as plt
+import numpy as np
+import pycuda
+from pycuda import gpuarray
+import pycuda.autinit
